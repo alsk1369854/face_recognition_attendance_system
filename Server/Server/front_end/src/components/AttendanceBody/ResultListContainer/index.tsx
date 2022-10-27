4bdc9bb95836e54be8014874447cb97627e2c6c3
alsk1369854 <alsk1369854@gmail.com>
2022-10-28 00:20:45 +0800

    update 2022/10/28, Front_end: update attendance page body

--- Server/Server/front_end/src/components/AttendanceBody/ResultListContainer/index.tsx
+++ Server/Server/front_end/src/components/AttendanceBody/ResultListContainer/index.tsx
@@ -2,10 +2,39 @@ import React from 'react'
 
 import style from './index.module.scss'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSort } from '@fortawesome/free-solid-svg-icons' // <-- import styles to be used
+
+
 export default function ResultListContainer() {
   return (
     <div className={style.result_list_container}>
-      
+      <div className={style.title_container}>
+        <div className={style.title_header}>
+          查詢結果
+        </div>
+      </div>
+      <div className={style.table_header}>
+        <div className={style.talbe_header_office_name}>
+          office_name
+        </div>
+        <div className={style.table_header_person_id}>
+          <FontAwesomeIcon icon={faSort} />
+          person_id
+        </div>
+        <div className={style.table_header_person_name}>
+          person_name
+        </div>
+        <div className={style.table_header_attendance_record}>
+          attendance_record
+        </div>
+      </div>
+      <div className={style.table_item_container}>
+        <div className={style.table_item}>
+          itme1
+        </div>
+
+      </div>
     </div>
   )
 }
